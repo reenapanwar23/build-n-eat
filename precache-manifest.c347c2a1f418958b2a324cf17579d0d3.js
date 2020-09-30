48292c0b4741ef241fecff8b0721b15874d80cd6
Reena <reenapanwar.23@gmail.com>
2020-09-30 13:55:51 +0530

    Updates

---
+++ precache-manifest.c347c2a1f418958b2a324cf17579d0d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e08d8a19326fb76fcb5abe2e7e4488e",
+    "url": "/build-n-eat/index.html"
+  },
+  {
+    "revision": "27e53c30ea75fa55ad76",
+    "url": "/build-n-eat/static/css/main.55a1118f.chunk.css"
+  },
+  {
+    "revision": "2cfd8d17d16a5f71ae94",
+    "url": "/build-n-eat/static/js/2.5e013c71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build-n-eat/static/js/2.5e013c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e53c30ea75fa55ad76",
+    "url": "/build-n-eat/static/js/main.3760e1f8.chunk.js"
+  },
+  {
+    "revision": "a14a1bf5535fcd667d3b",
+    "url": "/build-n-eat/static/js/runtime-main.c198d31a.js"
+  },
+  {
+    "revision": "a94ad3290c1534fd7d6ef28c42f40ba4",
+    "url": "/build-n-eat/static/media/BackgroundImage.a94ad329.jpg"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/build-n-eat/static/media/Burger-Logo.b8503d26.png"
+  }
+]);
